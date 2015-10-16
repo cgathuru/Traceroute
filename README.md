35ce7b2eef9505b1bf3caaf3c383c044023f9589
Dimitrios GALLOS <dgallo6@trusty>
2015-10-16 17:43:19 -0400

    Added ability for script to be run for a specified frequency by the user and for a specified duration.

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ This project performs a traceroute from the computer to a destination that's sup
 line arguments. The traceroute is performed by the operating systems implementation to get around
 firewall issues and permission issues that may arise from trying to open a socket or trying to
 receive an  ICMP packet.
+
+Usage: 
+download and save the python file 
+make the python file excecutable by :
+chmod + x traceroute.py 
+takes for command line parameters 
+1- frequency which can be any integer 
+2- frequency unit which can be any of s, m , h , d standing for second, minute, hour, day respectively 
+3- duration which can be any integer 
+4- duration unit which has the same options as the frequency unit
+
+example usage: 
+python traceroute.py 1 m 10 d 
+This will run traceroute every minute for 10 days 
--- traceroute.py
+++ traceroute.py
@@ -4,7 +4,7 @@ import subprocess
 import re
 import csv
 import time
-import statistics
+#import statistics
 
 __author__ = 'Charles'
 
@@ -12,12 +12,80 @@ __author__ = 'Charles'
 def main():
     domain = sys.argv[1] if len(sys.argv) > 1 else "www.google.ca"
     frequency = sys.argv[2] if len(sys.argv) > 2 else 1
-    multiplicity = sys.argv[3] if len(sys.argv) > 3 else 1
+    frequency = int(frequency)
+    frequency_unit = sys.argv[3] if len(sys.argv) > 3 else "s"
+    duration = sys.argv[4] if len(sys.argv) > 4 else 1
+    duration = int(duration)
+    duration_unit = sys.argv[5] if len(sys.argv) > 5 else "m"
     command = 'tracert'
     if platform.system() != 'Windows':
         command = 'traceroute'
 
     print('command is ' + command)
+
+    #get the time 
+    time_to_sleep = 0 
+    if frequency_unit == "s":
+        time_to_sleep = frequency 
+    elif frequency_unit == "m":
+        time_to_sleep = frequency * 60
+    elif frequency_unit == "h":
+        time_to_sleep = frequency * 3600
+    elif frequency_unit == "d": 
+        time_to_sleep = frequency * 3600 * 24
+
+    iteration_count = 0 
+    if duration_unit == "s":
+        if frequency_unit == "s":
+            iteration_count = duration / frequency
+        else:
+            print("Bad unit combination\n")
+            return 1
+    elif duration_unit == "m":
+        if frequency_unit == "s":
+            iteration_count = 60 * duration / frequency
+        elif frequency_unit == "m":
+            iteration_count = duration / frequency
+        else:
+            print("Bad unit combination \n")
+            return 1
+    elif duration_unit == "h":
+        if frequency_unit == "s":
+            iteration_count = 3600 * duration / frequency
+        elif frequency_unit == "m":
+            iteration_count = 60 * duration / frequency
+        elif frequency_unit == "h":
+            iteration_count = duration/frequency
+        else:
+            print("Bad unit combination \n")
+            return 1
+    elif duration_unit == "d":
+        if frequency_unit == "s":
+            iteration_count = 24*3600 * duration / frequency
+        elif frequency_unit == "m":
+            iteration_count = 24* 60 * duration / frequency
+        elif frequency_unit == "h":
+            iteration_count = 24* duration/frequency
+        elif frequency_unit == "d":
+            iteration_count = duration/frequency
+        else:
+            print("Bad unit combination \n")
+            return 1
+    else: 
+        print("bad input \n")
+        return 1
+
+    print ("Traceroute will run every " + str(time_to_sleep) +" seconds " + str(iteration_count) + " times \n")
+
+    for x in range (0, iteration_count):
+        get_traceroute_output(command, domain)
+        time.sleep(time_to_sleep)
+
+
+
+
+def get_traceroute_output(command, domain):
+
     output = subprocess.check_output([command, domain])
     decode_out = output.decode("utf-8")
     lines = decode_out.split('\n')
@@ -57,6 +125,7 @@ def main():
         print(ips)
         writer.writerow(['1', time.strftime("%x"), time.strftime("%X"), domain, ', '.join(ip_list),
                          ', '.join(avg_ttl), hops])
+    return
 
 
 if __name__ == '__main__':
