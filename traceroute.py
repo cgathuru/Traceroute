ac2d72e0b74c7926c742c084c376a6da890e4574
Dimitrios GALLOS <dgallo6@trusty>
2015-10-27 15:31:47 -0400

    new version of traceroute

--- traceroute.py
+++ traceroute.py
@@ -84,51 +84,25 @@ def main():
 
 
 
-def get_traceroute_output(command, domain):
+def get_traceroute_output(command, host):
 
-    output = subprocess.check_output([command, domain])
-    decode_out = output.decode("utf-8")
-    lines = decode_out.split('\n')
-    # p = re.compile('(\d)|(\d+\sms)|([A-za-z\.]+)|(\*)|(\[[0-9\\.]+\])')
-    ip_p = "((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])[ (\[]?(\.|dot)[ )\]]?){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]))"
-    time_pattern = "\d+\sms"
-    hops_p = "\d[\s]{2,}"
-    i = 0
-    hops = 0
-    ip_list = list()
-    avg_ttl = list()
-    for line in lines:
-        if line:
-            print("Line {}".format(i) + " " + line)
-            if len([match[0] for match in re.findall(hops_p, line)]) > 0:
-                ip = [match[0] for match in re.findall(ip_p, line)]
-                # ip_list.append(str(ip).strip('[]').strip('\'\''))
-                if len(ip) > 0 and ip != '*':
-                    print('Trying to append {}'.format(ip[0]))
-                    ip_list.append(ip[0])
-                    print("IP is " + str(ip[0]))
-                times = re.findall(time_pattern, line)
-                if len(times) > 0:
-                    avg_ttl.append(times[0].rstrip('ms'))
-                hops += 1
-                print('Found {} ip matches'.format(len(ip)))
-                print('Found {} time matches'.format(len(times)))
-        i += 1
+	with open("results.txt", "a") as output:
+		print ("Tracing", host)
 
-    print("Total hops: {}".format(hops))
-    print("Ips are:")
-    print(ip_list)
-    print("Times are:")
-    # avg_ttl = list(map(int, avg_ttl))
+		trace = subprocess.Popen([command, "-w", "100", host], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-    with open(domain + '.csv', 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        writer.writerow(['Route Number', 'Date', 'Time', 'Destination', 'Route', 'Route TTL', 'Num Hops'])
-        ips = ', '.join(ip_list)
-        print(ips)
-        writer.writerow(['1', time.strftime("%x"), time.strftime("%X"), domain, ', '.join(ip_list),
-                         ', '.join(avg_ttl), hops])
-    return
+		while True:
+		    hop = trace.stdout.readline()
+
+		    if not hop: break
+
+		    print '-->', hop.strip()
+		    output.write(hop)
+
+		# When you pipe stdout, the doc recommends that you use .communicate()
+		# instead of wait()
+		# see: http://docs.python.org/2/library/subprocess.html#subprocess.Popen.wait
+		trace.communicate()
 
 
 if __name__ == '__main__':
